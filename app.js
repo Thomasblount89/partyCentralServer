a1cefaf945c6e204aaba4dd1970fe6860f2e4656
Thomas Blount <Thomasblount89@gmail.com>
2021-04-14 21:19:56 -0400

    resetting database

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/rsvp', controllers.rsvpcontroller)
 
 dbConnection
   .authenticate()
-  .then(() => dbConnection.sync())  
+  .then(() => dbConnection.sync({force:true}))  
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`[Server]: App is listening on ${process.env.PORT}`);
