a0825359ff38eae0c78ae3ac9d875e5301df5792
Thomas Blount <Thomasblount89@gmail.com>
2021-04-14 21:22:00 -0400

    adding tables back in

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use('/rsvp', controllers.rsvpcontroller)
 
 dbConnection
   .authenticate()
-  .then(() => dbConnection.sync({force:true}))  
+  .then(() => dbConnection.sync())  
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`[Server]: App is listening on ${process.env.PORT}`);
